b242b7590b4edb3a502dcde45177d5e922627516
random-raccoon <93792079+random-raccoon@users.noreply.github.com>
2022-12-11 16:50:23 +0000

    Day 5 all done.

---
+++ 05/part1.py
@@ -0,0 +1,34 @@
+# Thank goodness we're limiting to single digit column numbers!
+import re
+
+with open('05/input.txt', 'r') as f:
+    line = f.readline()
+    numStacks = len(line) // 4
+    stacks = [[] for i in range(numStacks)]
+
+    # Read in the contents of the stacks.
+    while line != '\n':
+        for i in range(numStacks):
+            c = line[i * 4 + 1]
+            if c != ' ':
+                stacks[i].append(c)
+        line = f.readline()
+
+    # Remove the stack numbers
+    stacks = [stack[:-1] for stack in stacks]
+    # Reverse them so that the top of the stack is the end of the list.
+    for stack in stacks:
+        stack.reverse()
+
+    instructions = f.readlines()
+    for instruction in instructions:
+        parts = re.search('move (.*) from (.) to (.)', instruction).groups()
+        num = int(parts[0])
+        source = int(parts[1]) - 1
+        dest = int(parts[2]) - 1
+
+        for i in range(num):
+            stacks[dest].append(stacks[source].pop())
+
+    message = ''.join([stack[-1] for stack in stacks])
+    print(message)
